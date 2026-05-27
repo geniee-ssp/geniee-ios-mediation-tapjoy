7f544db86f00d743f949b97d506e96bfa593ab92
Nguyen Duy Long <longnd@geniee.co.jp>
2026-05-27 16:22:51 +0700

    Update SPM URLs from unigeniee to geniee-ssp

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/unigeniee/swift-package-manager-geniee-ios-sdk.git",
+            url: "https://github.com/geniee-ssp/swift-package-manager-geniee-ios-sdk.git",
             from: "8.4.3"
         ),
         .package(
@@ -32,7 +32,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GenieeMediationTapjoy",
-            url: "https://github.com/unigeniee/geniee-ios-mediation-tapjoy/releases/download/14.7.0/GenieeMediationTapjoy.14.7.0.0.zip",
+            url: "https://github.com/geniee-ssp/geniee-ios-mediation-tapjoy/releases/download/14.7.0/GenieeMediationTapjoy.14.7.0.0.zip",
             checksum: "0d63c6279d209ec4b96ae2246c10d5aeb8f808bc24709d4f208ea73d239dd32e"
         )
     ]
